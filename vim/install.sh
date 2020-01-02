d79c1867f9303c6082d7e978d6bb88c0ff293155
FioraPellerin <ifthe1201@naver.com>
2020-01-02 16:57:12 +0900

    vim 설정

---
+++ vim/install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -x
+
+CURFILE=$(readlink -f "$0")
+CURPATH=$(dirname "$CURFILE")
+
+sudo add-apt-repository ppa:jonathonf/vim
+sudo apt update
+sudo apt install vim
+
+rm -f ~/.vimrc
+rm -rf ~/.vim
+ln -s "${CURPATH}/vimrc" ~/.vimrc 
+ln -s "${CURPATH}/vim" ~/.vim
+mkdir ~/.vimhistory
+touch ~/.vimhistory/vimhis.txt
+
+vi -c 'PluginInstall' -c 'q!' -c 'q!'
